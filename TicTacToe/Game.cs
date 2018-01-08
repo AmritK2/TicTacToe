a0118a30a4c258378f267fd4aa46059d81884f8c
AmritK2 <amrit.kaur@myob.com>
2018-01-09 10:23:29 +1300

    split the logic into a different class

---
+++ TicTacToe/Game.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+    public class Game
+    {
+        public string ReturnOutputBoard(string letter, string coord, string inputBoard)
+        {
+            var outputBoard = string.Empty;
+            var position = coord.Split(',');
+            var arrayBoard = inputBoard.Split('\n');
+            for (var row = 0; row < arrayBoard.Length; row++)
+            {
+                for (int column = 0; column < arrayBoard.Length; column++)
+                {
+                    if (row == int.Parse(position[0]) - 1 && column == int.Parse(position[1]) - 1)
+                    {
+                        outputBoard += letter;
+                    }
+                    else
+                    {
+                        outputBoard += arrayBoard[row][column];
+                    }
+
+
+                }
+                outputBoard += "\n";
+
+            }
+
+            return outputBoard.TrimEnd('\n');
+        }
+    }
+}
--- TicTacToe/GameShould.cs
+++ TicTacToe/GameShould.cs
@@ -30,41 +30,21 @@ namespace TicTacToe
                   "...\n" +
                   "...")]
 
+        [TestCase("...\n" +
+                  "...\n" +
+                  "...",
+                  "X",
+                  "2,2",
+
+                  ".X.\n" +
+                  "...\n" +
+                  "...")]
+
         public void ShouldReturnOutputBoard(string inputBoard, string letter, string coord, string outputBoard)
         {
             var game = new Game();
             var result = game.ReturnOutputBoard(letter, coord, inputBoard);
             Assert.AreEqual(outputBoard, result);
-        }
-    }
-
-    public class Game
-    {
-        public string ReturnOutputBoard(string letter, string coord, string inputBoard)
-        {
-            var outputBoard = string.Empty;
-            var position = coord.Split(',');
-            var arrayBoard = inputBoard.Split('\n');
-            for (var row = 0; row < arrayBoard.Length; row++)
-            {
-                for (int column = 0; column < arrayBoard.Length; column++)
-                {
-                    if (row == int.Parse(position[0]) - 1 && column == int.Parse(position[1]) - 1)
-                    {
-                        outputBoard += letter;
-                    }
-                    else
-                    {
-                        outputBoard += arrayBoard[row][column];
-                    }
-
-                    
-                }
-                outputBoard += "\n";
-
-            }
-            
-            return outputBoard.TrimEnd('\n');
         }
     }
 }
